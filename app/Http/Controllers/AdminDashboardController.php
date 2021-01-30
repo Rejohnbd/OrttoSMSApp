25cd7026f52992ded17efdfc8f3326290dd0ae3e
unknown <rejohn.cse@gmail.com>
2021-01-30 16:15:42 +0600

    Create Divison migrate, model and save to db. Create User seed

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CsvUpload\CsvUploadRequest;
+use App\Models\Division;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class AdminDashboardController extends Controller
 {
@@ -21,11 +23,57 @@ class AdminDashboardController extends Controller
     {
         $file_extension = $request->file_name->getClientOriginalExtension();
         if ($file_extension == 'csv') {
-            dd('CSV Extension');
+            if ($request->csv_file_type == 1) {
+                $this->divisionCsvUpload($request);
+                session()->flash('success', 'Division List Uploaded Successfully');
+                return redirect()->route('csv_import');
+            }
         } else {
             session()->flash('error', 'Please Provide valid CSV file');
             return redirect()->route('csv_import');
         }
         dd($request->file_name->getClientOriginalExtension());
+    }
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+
+    public function divisionCsvUpload(Request $request)
+    {
+        $currentDate = date('Y_m_d_H_i');
+        $orginalName =  preg_replace("/\s+/", "_", $request->file('file_name')->getClientOriginalName());
+        $fileName = $currentDate . '_' . $orginalName;
+        $file = $request->file('file_name');
+        $location = 'uploads';
+        $file->move($location . '/', $fileName);
+        $filepath = public_path($location . "/" . $fileName);
+        $uploadFile = fopen($filepath, "r");
+
+        $importData_arr = array();
+        $i = 0;
+        while (($filedata = fgetcsv($uploadFile)) !== false) {
+            $num = count($filedata);
+            // Skip first row 
+            if ($i == 0) {
+                $i++;
+                continue;
+            }
+            for ($c = 0; $c < $num; $c++) {
+                $importData_arr[$i][] = $filedata[$c];
+            }
+            $i++;
+        }
+        fclose($uploadFile);
+
+        foreach ($importData_arr as $importData) {
+            Division::create([
+                'division' => $importData[0]
+            ]);
+        }
+        $unlinkLink = public_path('uploads/' . $fileName);
+        unlink($unlinkLink);
     }
 }
