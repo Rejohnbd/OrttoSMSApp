a78fe8c1fc8dd9c57a86aa181ba1f30546f53af6
unknown <rejohn.cse@gmail.com>
2021-01-30 16:43:51 +0600

    Fix some issue for district controller & migration

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CsvUpload\CsvUploadRequest;
+use App\Models\District;
 use App\Models\Division;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
@@ -28,18 +29,17 @@ class AdminDashboardController extends Controller
                 session()->flash('success', 'Division List Uploaded Successfully');
                 return redirect()->route('csv_import');
             }
+            if ($request->csv_file_type == 2) {
+                $this->districtCsvUpload($request);
+                session()->flash('success', 'District List Uploaded Successfully');
+                return redirect()->route('csv_import');
+            }
         } else {
             session()->flash('error', 'Please Provide valid CSV file');
             return redirect()->route('csv_import');
         }
         dd($request->file_name->getClientOriginalExtension());
     }
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
 
     public function divisionCsvUpload(Request $request)
     {
@@ -71,6 +71,43 @@ class AdminDashboardController extends Controller
         foreach ($importData_arr as $importData) {
             Division::create([
                 'division' => $importData[0]
+            ]);
+        }
+        $unlinkLink = public_path('uploads/' . $fileName);
+        unlink($unlinkLink);
+    }
+
+    public function districtCsvUpload(Request $request)
+    {
+        $currentDate = date('Y_m_d_H_i');
+        $orginalName =  preg_replace("/\s+/", "_", $request->file('file_name')->getClientOriginalName());
+        $fileName = $currentDate . '_' . $orginalName;
+        $file = $request->file('file_name');
+        $location = 'uploads';
+        $file->move($location . '/', $fileName);
+        $filepath = public_path($location . "/" . $fileName);
+        $uploadFile = fopen($filepath, "r");
+
+        $importData_arr = array();
+        $i = 0;
+        while (($filedata = fgetcsv($uploadFile)) !== false) {
+            $num = count($filedata);
+            // Skip first row 
+            if ($i == 0) {
+                $i++;
+                continue;
+            }
+            for ($c = 0; $c < $num; $c++) {
+                $importData_arr[$i][] = $filedata[$c];
+            }
+            $i++;
+        }
+        fclose($uploadFile);
+
+        foreach ($importData_arr as $importData) {
+            District::create([
+                'division_id' => $importData[0],
+                'district' => $importData[1]
             ]);
         }
         $unlinkLink = public_path('uploads/' . $fileName);
--- database/migrations/2021_01_30_101848_create_districts_table.php
+++ database/migrations/2021_01_30_101848_create_districts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDivisionsTable extends Migration
+class CreateDistrictsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,10 @@ class CreateDivisionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('divisions', function (Blueprint $table) {
+        Schema::create('districts', function (Blueprint $table) {
             $table->id();
-            $table->string('division', 30);
+            $table->unsignedBigInteger('division_id')->comment('Fk of Devision Table');
+            $table->string('district', 30);
             $table->timestamps();
         });
     }
@@ -27,6 +28,6 @@ class CreateDivisionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('divisions');
+        Schema::dropIfExists('districts');
     }
 }
